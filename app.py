99337707255f0000804e7c71d2ec239b625b73ec
scoutskylar <scoutskylar@outlook.com>
2023-10-09 11:49:04 -0400

    Create front end for root page
    * Prompt and response boxes
    * Response/output only shown on post request
    * Added favicon in standard sizes
    * Basic stylesheet

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import *
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def root():
+    if request.method == "POST":
+        # For now, the output is just the input
+        output = escape(request.form['input'])
+        return render_template('index.html', output=output, outputDisplay='block')
+    else:
+        return render_template('index.html', output='', outputDisplay='none')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.root_path, 'static/favicon.ico', mimetype='image/vnd.microsoft.icon')
