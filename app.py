a83f9c29cb00821bcd8e82c2e532f3df8e83d7a7
KedricNees01 <97705596+KedricNees01@users.noreply.github.com>
2023-10-16 10:41:49 -0400

    Push to Root backend (#15)
    * Add meta elements to index
    
    * Create files to test API
    
    * Update app.py
    
    Updated the three files to test and try out different things relating to an API.
    
    * Update app.py
    
    Update app.py to incorporate the languagemodels python module
    
    * Add basic llm functionaility
    
    * Add basic dynamid ram allocation
    
    * Run black formatter
    
    * Delete api_test.py
    
    * Delete requests_test.py
    
    ---------
    
    Co-authored-by: shama2025 <2003mashaffer@gmail.com>

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+from html import escape
 from flask import *
+import languagemodels as lm
 from markupsafe import escape
+import requests
+import psutil
 
 app = Flask(__name__)
 
 
+# Front end
 @app.route("/", methods=["GET", "POST"])
 def root():
     if request.method == "POST":
         # For now, the output is just the input
         output = escape(request.form["input"])
-        return render_template("index.html", output=output, outputDisplay="block")
+        api_route = requests.get("http://127.0.0.1:5000/api?output= " + f"{output}")
+        return render_template(
+            "index.html", output=api_route.json(), outputDisplay="block"
+        )
     else:
         return render_template("index.html", output="", outputDisplay="none")
 
@@ -19,3 +27,18 @@ def favicon():
     return send_from_directory(
         app.root_path, "static/favicon.ico", mimetype="image/vnd.microsoft.icon"
     )
+
+
+# Backend
+
+
+@app.route("/api")
+def api():
+    chat = request.args.get("output", "")
+
+    # changes ram the llm is using dynamic to half of avaiable ram to imporve accuracy
+    freeRam = psutil.virtual_memory().free
+    lm.set_max_ram(freeRam / 2)
+
+    reply = lm.do(chat)
+    return {"data": f"{reply}"}, 200
