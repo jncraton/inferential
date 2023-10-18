4646e177a339fc57148b33806b889bcc71887681
shama2025 <2003mashaffer@gmail.com>
2023-10-18 12:42:26 -0400

    Update test_main.py
    Test will check if the word France is in the response

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from app import *
-
+from playwright.sync_api import *
 
 @pytest.fixture()
 def client():
@@ -10,3 +10,11 @@ def client():
 
 def test_placeholder():
     assert True
+
+def test_paris(page:Page):
+    page.goto("http://127.0.0.1:5000/")
+    page.get_by_label("Prompt").click()
+    page.get_by_label("Prompt").fill("Where is Paris")
+    page.get_by_role("button", name="Submit").click() 
+    output_value = page.locator(".output").inner_text()
+    assert("France" in output_value)
