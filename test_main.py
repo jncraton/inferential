12c10833b1f75d1be4532f533fb709028768bf10
shama2025 <2003mashaffer@gmail.com>
2023-10-18 13:43:27 -0400

    Update test_main.py
    Ran black-fix

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ import pytest
 from app import *
 from playwright.sync_api import *
 
+
 @pytest.fixture()
 def client():
     app.config["TESTING"] = True
@@ -11,24 +12,27 @@ def client():
 def test_placeholder():
     assert True
 
-def test_paris(page:Page):
+
+def test_paris(page: Page):
     page.goto("http://127.0.0.1:5000/")
     page.get_by_label("Prompt").click()
     page.get_by_label("Prompt").fill("Where is Paris")
-    page.get_by_role("button", name="Submit").click() 
+    page.get_by_role("button", name="Submit").click()
     output_value = page.locator(".output").inner_text()
-    assert("France" in output_value)
+    assert "France" in output_value
 
-def test_empty_chat(page:Page):
+
+def test_empty_chat(page: Page):
     page.goto("http://127.0.0.1:5000/")
-    page.get_by_role("button", name="Submit").click() 
+    page.get_by_role("button", name="Submit").click()
     output_value = page.locator(".output").inner_text()
-    assert("Enter a valid input!" in output_value)
+    assert "Enter a valid input!" in output_value
 
-def test_chat_too_big(page:Page):
+
+def test_chat_too_big(page: Page):
     page.goto("http://127.0.0.1:5000/")
     page.get_by_label("Prompt").click()
     chat_input = page.locator(".input").inner_text()
-    page.get_by_role("button", name="Submit").click() 
+    page.get_by_role("button", name="Submit").click()
     output_value = page.locator(".output").inner_text()
-    assert(("Enter a valid input!" in output_value) and len(str(chat_input)) >= 500 )
+    assert ("Enter a valid input!" in output_value) and len(str(chat_input)) >= 500
