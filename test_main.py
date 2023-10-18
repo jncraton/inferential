7cd07bd2817e7cd6fe59f30df5a5945d4d8e49e3
shama2025 <2003mashaffer@gmail.com>
2023-10-18 13:38:11 -0400

    Update test_main.py
    Created a test for an empty chat and one if the chat exceeds 500 characters

--- test_main.py
+++ test_main.py
@@ -18,3 +18,17 @@ def test_paris(page:Page):
     page.get_by_role("button", name="Submit").click() 
     output_value = page.locator(".output").inner_text()
     assert("France" in output_value)
+
+def test_empty_chat(page:Page):
+    page.goto("http://127.0.0.1:5000/")
+    page.get_by_role("button", name="Submit").click() 
+    output_value = page.locator(".output").inner_text()
+    assert("Enter a valid input!" in output_value)
+
+def test_chat_too_big(page:Page):
+    page.goto("http://127.0.0.1:5000/")
+    page.get_by_label("Prompt").click()
+    chat_input = page.locator(".input").inner_text()
+    page.get_by_role("button", name="Submit").click() 
+    output_value = page.locator(".output").inner_text()
+    assert(("Enter a valid input!" in output_value) and len(str(chat_input)) >= 500 )
