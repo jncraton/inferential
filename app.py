b0fed628397af70af46492f73c81f6282983cca8
Nate Martin <natemartin03@gmail.com>
2023-10-24 10:46:05 -0400

    Changed spacing to the code and comments to make things a little better

--- app.py
+++ app.py
@@ -4,17 +4,15 @@ import languagemodels as lm
 from markupsafe import escape
 import requests
 
-
 app = Flask(__name__)
 
-
-# Front end
+#Front end
 @app.route("/", methods=["GET", "POST"])
 def root():
     if request.method == "POST":
         # For now, the output is just the input
         output = escape(request.form["input"])
-        api_route = requests.get("http://127.0.0.1:5000/api?output= " + f"{output}")
+        api_route = requests.get("http://127.0.0.1:5000/api?output=" + f"{output}")
 
         response = api_route.json()
         return render_template(
@@ -23,22 +21,18 @@ def root():
     else:
         return render_template("index.html", output="", outputDisplay="none")
 
-
 @app.route("/favicon.ico")
 def favicon():
     return send_from_directory(
         app.root_path, "static/favicon.ico", mimetype="image/vnd.microsoft.icon"
     )
 
-
-# Backend
-
-
+#Backend
 @app.route("/api")
 def api():
     query = request.args.get("output", "")
-    if len(query) >= 250 or query == " ":
+    if len(query) >= 250 or query == "":
         return {"data": "Enter a valid query!"}
 
     reply = lm.do(query)
-    return {"data": reply}, 200  # returns the dictionary and a 200 response code
+    return {"data": reply}, 200  #Returns the dictionary and a 200 response code
