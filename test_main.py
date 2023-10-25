a52261664716cc6a74b050efc0b4e18cfb2c299e
shama2025 <2003mashaffer@gmail.com>
2023-10-25 11:03:35 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from app import *
-from playwright.sync_api import *
+from playwright.sync_api import * 
 from random import choice
 from string import ascii_lowercase
 
@@ -16,15 +16,15 @@ def test_paris_query(page: Page):
     page.get_by_label("Prompt").click()
     page.get_by_label("Prompt").fill("Where is Paris")
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "France" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("France")
 
 
 def test_empty_query(page: Page):
     page.goto("http://127.0.0.1:5000/")
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "Enter a valid query!" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("Enter a valid query!")
 
 
 def test_query_too_big(page: Page):
@@ -34,5 +34,5 @@ def test_query_too_big(page: Page):
     query = "a".join(choice(ascii_lowercase) for i in range(n))
     page.get_by_label("Prompt").fill(query)
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "Enter a valid query!" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("Enter a valid query!")
