343104a7eb742ff71683a2ab6a3d8fa1d00909a9
Nate Martin <natemartin03@gmail.com>
2023-10-26 09:40:34 -0400

    Fixed pytest

--- test_main.py
+++ test_main.py
@@ -11,25 +11,22 @@ def client():
     return app.test_client()
 
 
-# Tests the LLM model is smart enough to say Paris is in France
 def test_paris_query(page: Page):
     page.goto("http://127.0.0.1:5000/")
     page.get_by_label("Prompt").click()
     page.get_by_label("Prompt").fill("Where is Paris")
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "France" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("France")
 
 
-# Tests for correct response when prompted with an empty query
 def test_empty_query(page: Page):
     page.goto("http://127.0.0.1:5000/")
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "Enter a valid query!" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("Enter a valid query!")
 
 
-# Tests for the correct response when given a query too big
 def test_query_too_big(page: Page):
     n = 250
     page.goto("http://127.0.0.1:5000/")
@@ -37,5 +34,5 @@ def test_query_too_big(page: Page):
     query = "a".join(choice(ascii_lowercase) for i in range(n))
     page.get_by_label("Prompt").fill(query)
     page.get_by_role("button", name="Submit").click()
-    output_value = page.locator(".output").inner_text()
-    assert "Enter a valid query!" in output_value
+    chat_reply = page.locator(".output")
+    expect(chat_reply).to_contain_text("Enter a valid query!")
