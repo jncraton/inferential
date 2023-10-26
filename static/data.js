cbcfc6df26303b6d3519b1dfbaecbbac77173cb2
scoutskylar <scoutskylar@outlook.com>
2023-10-26 10:13:47 -0400

    Show error messages from API

--- static/data.js
+++ static/data.js
@@ -5,13 +5,10 @@ const input = document.getElementById('input')
 
 // event listener on the button element
 button.onclick = function () {
+  output.innerText = 'Loading...'
   fetch('/api?' + new URLSearchParams({ input: input.value }))
     .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        alert('something is wrong')
-      }
+      return response.json()
     })
     .then(json => {
       output.innerText = json.data
