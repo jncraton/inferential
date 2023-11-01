dfeaf9ffa3567445cdd906ace71aea51038a8c0c
Nate Martin <natemartin03@gmail.com>
2023-11-01 10:39:12 -0400

    Merged to main and ran prettier

--- static/data.js
+++ static/data.js
@@ -5,7 +5,8 @@ const input = document.getElementById('input')
 
 // Assign different class to output based on response
 function setOutputClass(outputString) {
-  if (outputString.length < 80) {         //Number of characters before it switches styles is 80, change if needed
+  if (outputString.length < 80) {
+    //Number of characters before it switches styles is 80, change if
     output.className = 'output-simple'
   } else {
     output.className = 'output'
@@ -14,6 +15,7 @@ function setOutputClass(outputString) {
 
 // Event listener on the button element
 button.onclick = function () {
+  output.innerText = 'Loading...'
   fetch('/api?' + new URLSearchParams({ input: input.value }))
     .then(response => {
       if (response.ok) {
