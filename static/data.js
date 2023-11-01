6823626eca70f83f9b7e4a8a660746d5eca300ce
Josh Westrum <jcwestrum@anderson.edu>
2023-11-01 15:11:54 -0400

    Fix pytest error

--- static/data.js
+++ static/data.js
@@ -3,8 +3,18 @@ const button = document.getElementById('submitButton')
 const output = document.getElementById('outputResponse')
 const input = document.getElementById('input')
 
-// event listener on the button element
-const buttonClick = e => {
+// Event Listners
+button.addEventListener('click', submitButton)
+
+input.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter' && !e.shiftKey) {
+    e.preventDefault()
+    submitButton()
+  }
+})
+
+// Function calls fetch API upon promptSubmition
+function submitButton() {
   output.innerText = 'Loading...'
   fetch('/api?' + new URLSearchParams({ input: input.value }))
     .then(response => {
@@ -15,10 +25,3 @@ const buttonClick = e => {
     })
     .catch(err => console.error(err))
 }
-
-input.addEventListener('keydown', function (e) {
-  if (e.key === 'Enter' && !e.shiftKey) {
-    e.preventDefault()
-    buttonClick()
-  }
-})
