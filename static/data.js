5ea88a428a20a213a4a1908c0e64e7711a75f1a1
Mason Wallace <mdwallace2@anderson.edu>
2023-11-01 19:06:17 -0400

    Shift enter (#34)
    * Add enter and shift enter
    
    * Add enter and shift-enter
    
    * Update test_main.py
    
    * Saving my changes before switching branches
    
    * Test
    
    * Fix form submit
    
    * move to .js
    
    * fix preventDefault
    
    * Add enter tests
    
    * ran prettier and black
    
    * Added comments to tests
    
    * remove files
    
    * Delete File
    
    * Made code simpler
    
    * run prettier
    
    * Fix pytest error
    
    ---------
    
    Co-authored-by: shama2025 <2003mashaffer@gmail.com>
    Co-authored-by: Josh Westrum <jcwestrum@anderson.edu>

--- static/data.js
+++ static/data.js
@@ -3,8 +3,18 @@ const button = document.getElementById('submitButton')
 const output = document.getElementById('outputResponse')
 const input = document.getElementById('input')
 
-// event listener on the button element
-button.onclick = function () {
+// Event Listners
+button.addEventListener('click', submitButton)
+
+input.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter' && !e.shiftKey) {
+    e.preventDefault()
+    submitButton()
+  }
+})
+
+// Function calls fetch API upon promptSubmition
+function submitButton() {
   output.innerText = 'Loading...'
   fetch('/api?' + new URLSearchParams({ input: input.value }))
     .then(response => {
