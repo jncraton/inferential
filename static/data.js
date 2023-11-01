4af3afb5e365d67f047a1e33ccc189b7a35b5bf0
Nate Martin <natemartin03@gmail.com>
2023-11-01 10:37:10 -0400

    Added function that switches between styles for output

--- static/data.js
+++ static/data.js
@@ -3,15 +3,29 @@ const button = document.getElementById('submitButton')
 const output = document.getElementById('outputResponse')
 const input = document.getElementById('input')
 
-// event listener on the button element
+// Assign different class to output based on response
+function setOutputClass(outputString) {
+  if (outputString.length < 80) {         //Number of characters before it switches styles is 80, change if needed
+    output.className = 'output-simple'
+  } else {
+    output.className = 'output'
+  }
+}
+
+// Event listener on the button element
 button.onclick = function () {
-  output.innerText = 'Loading...'
   fetch('/api?' + new URLSearchParams({ input: input.value }))
     .then(response => {
-      return response.json()
+      if (response.ok) {
+        return response.json()
+      } else {
+        alert('Something is wrong')
+      }
     })
     .then(json => {
       output.innerText = json.data
+
+      setOutputClass(json.data)
     })
     .catch(err => console.error(err))
 }
