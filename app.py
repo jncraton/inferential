343f01df79b19fd90a176b49e0c0bd11b087beeb
Caburke1218 <caburke2@anderson.edu>
2023-11-13 09:37:43 -0500

    initial changes in config file and app.py for package support

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
-from html import escape
 from flask import *
-import languagemodels as lm
-from markupsafe import escape
-import requests
 import yaml
+from ctransformers import AutoModelForCausalLM
+import ctranslate2
 
 
 app = Flask(__name__)
 
-# Opens the config file and assigns it to configIndex
+# Opens the config file and assigns it to config_index
 with open("config.yml", "r") as f:
-    configIndex = yaml.safe_load(f)
+    config_index = yaml.safe_load(f)
 
 # Uses the chosen model in the config file and sets it to selected_model
-selected_model = configIndex["models"][0]
+selected_model = config_index["models"][2]
 
-# Changes the model lm uses to the selected model.
-lm.config["instruct_model"] = selected_model
-
+# Dynamically load the appropriate model based on the selected backend
+if selected_model["backend"] == "ctransformers":
+    llm = AutoModelForCausalLM.from_pretrained(selected_model['name'])
+else:
+    pass
+    
 
 # Loading page
 @app.route("/")
 def loading_page():
     return render_template("status.html")
 
-
 # API Front End
 @app.route("/playground")
 def playground():
     return render_template("index.html")
-
 
 @app.route("/favicon.ico")
 def favicon():
     return send_from_directory(
         app.root_path, "static/favicon.ico", mimetype="image/vnd.microsoft.icon"
     )
-
 
 # Backend
 @app.route("/api")
@@ -47,5 +45,6 @@ def api():
     if len(query) >= 250:
         return {"data": "Error: The prompt was too long."}, 413  # 413 Content Too Large
 
-    reply = lm.do(query)
+    # Generate a reply using the selected model
+    reply = llm(query)
     return {"data": reply}, 200  # returns with a response code of 200 OK
