e48b50a86e928ba542606ab255deb6cd497c0281
scoutskylar <scoutskylar@outlook.com>
2023-11-14 10:19:36 -0500

    Move inference code to separate file

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-import os
 from flask import *
 from markupsafe import escape
-from huggingface_hub import hf_hub_download
-from tokenizers import Tokenizer
-import ctranslate2
-import yaml
+from inference import generate_response_ctranslate2
+from huggingface_hub import hf_hub_download, snapshot_download
 
 app = Flask(__name__)
 
-# Download tokenizer, model config, and vocabulary
-hf_hub_download("jncraton/LaMini-Flan-T5-248M-ct2-int8", "config.json")
-hf_hub_download("jncraton/LaMini-Flan-T5-248M-ct2-int8", "shared_vocabulary.txt")
+# Download the model (ctranslate2)
+model_folder = snapshot_download(repo_id="jncraton/LaMini-Flan-T5-248M-ct2-int8")
 tok_config = hf_hub_download("jncraton/LaMini-Flan-T5-248M-ct2-int8", "tokenizer.json")
-model_path = hf_hub_download("jncraton/LaMini-Flan-T5-248M-ct2-int8", "model.bin")
 
 
 # Loading page
@@ -43,30 +38,5 @@ def api():
     if len(query) >= 250:
         return "Error: The prompt was too long.", 413  # 413 Content Too Large
 
-    tokens = generate_response(query)
+    tokens = generate_response_ctranslate2(query, model_folder)
     return Response(tokens, content_type="text/plain")
-
-
-def generate_response(input):
-    # Tokenize the input
-    tokenizer = Tokenizer.from_file(tok_config)
-    input_tokens = tokenizer.encode(input).tokens
-
-    model_base_path = os.path.dirname(model_path)
-
-    # Initialize the translator
-    model = ctranslate2.Translator(model_base_path, compute_type="int8")
-
-    # Translate the tokens
-    results = model.generate_tokens(input_tokens, disable_unk=True)
-
-    accumlated_results = []
-    current_length = 0
-    for item in results:
-        if item.is_last:
-            break
-        accumlated_results.append(item.token_id)
-        decoded_string = tokenizer.decode(accumlated_results)
-        new_text = decoded_string[current_length - len(decoded_string) :]
-        current_length = len(decoded_string)
-        yield new_text
