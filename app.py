af38d37f3a53acf4bf4f2d90491c3fc65bd6eeb3
KedricNees01 <kedricnees@gmail.com>
2023-11-15 08:55:42 -0500

    save changes

--- app.py
+++ app.py
@@ -2,13 +2,26 @@ from flask import *
 from markupsafe import escape
 from inference import generate_response_ctranslate2
 from huggingface_hub import hf_hub_download, snapshot_download
+import yaml
+from ctransformers import AutoModelForCausalLM
 
 app = Flask(__name__)
+
+
+with open("config.yml", "r") as f:
+    config_index = yaml.safe_load(f)
+
+selected_model = config_index["models"][2]
+
 
 # Download the model (ctranslate2)
 model_folder = snapshot_download(repo_id="jncraton/LaMini-Flan-T5-248M-ct2-int8")
 tok_config = hf_hub_download("jncraton/LaMini-Flan-T5-248M-ct2-int8", "tokenizer.json")
 
+if selected_model["backend"] == "ctransformers":
+    llm = AutoModelForCausalLM.from_pretrained(selected_model["name"], gpu_layers=50)
+else:
+    pass
 
 # Loading page
 @app.route("/")
@@ -37,6 +50,13 @@ def api():
         return "Error: No prompt was provided.", 400  # 400 Bad Request
     if len(query) >= 250:
         return "Error: The prompt was too long.", 413  # 413 Content Too Large
+    
+    if selected_model["backend"] == "ctranslate2":
+        tokens = generate_response_ctranslate2(query, model_folder)
 
-    tokens = generate_response_ctranslate2(query, model_folder)
+    if selected_model["backend"] == "ctransformers":
+        llm = AutoModelForCausalLM.from_pretrained(selected_model["name"], gpu_layers=50)
+        for text in llm("Ctransformers will", stream=True):
+            return (text, end="", flush=True)
+
     return Response(tokens, content_type="text/plain")
