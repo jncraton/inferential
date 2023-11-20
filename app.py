cfccb05be13f2de82912918d057622c8a16ab075
shama2025 <2003mashaffer@gmail.com>
2023-11-19 19:40:28 -0500

    Add decision logic
    If statement will take into account if no model was selected

--- app.py
+++ app.py
@@ -11,15 +11,7 @@ app = Flask(__name__)
 with open("config.yml", "r") as f:
     config_index = yaml.safe_load(f)
 
-# Uses the chosen model in the config file and sets it to selected_model
-selected_model = config_index["models"][5]
-# Dynamically load the appropriate model based on the selected backend
-if selected_model["backend"] == "ctransformers":
-    llm = AutoModelForCausalLM.from_pretrained(selected_model["name"])
-else:
-    # Download the model (ctranslate2)
-    model_folder = snapshot_download(repo_id=selected_model["name"])
-    tok_config = hf_hub_download(selected_model["name"], "tokenizer.json")
+# # Uses the chosen model in the config file and sets it to selected_model
 
 
 # Loading page
@@ -45,13 +37,18 @@ def favicon():
 @app.route("/api")
 def api():
     query = request.args.get("input", "")
+    model = int(request.args.get("model", ""))
+
     if query == "":
         return "Error: No prompt was provided.", 400  # 400 Bad Request
     if len(query) >= 250:
         return "Error: The prompt was too long.", 413  # 413 Content Too Large
 
-    model = int(request.args.get("model", ""))
+    if model == -1:  # If no model is picked change to default
+        selected_model = config_index["models"][0]
+
     selected_model = config_index["models"][model]
+    # Dynamically load the appropriate model based on the selected backend
     if selected_model["backend"] == "ctransformers":
         llm = AutoModelForCausalLM.from_pretrained(selected_model["name"])
     else:
@@ -59,10 +56,10 @@ def api():
         model_folder = snapshot_download(repo_id=selected_model["name"])
         tok_config = hf_hub_download(selected_model["name"], "tokenizer.json")
 
-    # Dynamically generate reposnse based on the selected backend
     if selected_model["backend"] == "ctransformers":
         reply = generate_response_ctransformers(query, llm)
         return Response(reply, content_type="text/plain")
     else:
+        # Download the model (ctranslate2)
         tokens = generate_response_ctranslate2(query, model_folder)
         return Response(tokens, content_type="text/plain")
