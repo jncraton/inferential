411bfa7a1a65645273d1ef70cc98f8b4ae96e437
scoutskylar <scoutskylar@outlook.com>
2023-11-21 11:04:17 -0500

    Improvements to styling & markup

--- static/loading.js
+++ static/loading.js
@@ -1,29 +1,15 @@
-function initializePlayground() {
+function placeholderPlaygroundInitialization() {
   document.getElementById('status').textContent = 'Starting up web server'
   setTimeout(function () {
     document.getElementById('status').textContent = 'Downloading assets'
     setTimeout(function () {
       document.getElementById('status').textContent = 'Initializing server'
-      setTimeout(function () {
-        loaded()
-      }, 600)
+      setTimeout(loaded, 600)
     }, 1500)
   }, 450)
 }
+placeholderPlaygroundInitialization()
 
 function loaded() {
   document.getElementById('status').textContent = 'Assets loaded'
-  var Header = document.getElementById('Header')
-  Header.innerHTML = '<a href="http://127.0.0.1:5000/playground">Playground</a>'
 }
-
-//if (!localStorage.getItem('pageLoaded')) {
-// This will only run when the page is loaded for the first time
-initializePlayground()
-
-// Set the flag in localStorage so the if statement will not pass again when navigated back
-//localStorage.setItem('pageLoaded', 'true');
-//}
-//else {
-//loaded();
-//}
