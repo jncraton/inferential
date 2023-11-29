bd65d8f145e738924ad781924f46525d60aeeb76
Nate Martin <natemartin03@gmail.com>
2023-11-29 10:35:52 -0500

    Ran prettier

--- static/loading.js
+++ static/loading.js
@@ -1,57 +1,57 @@
 function placeholderPlaygroundInitialization() {
-    document.getElementById('status').textContent = 'Starting up web server'
+  document.getElementById('status').textContent = 'Starting up web server'
+
+  setTimeout(function () {
+    document.getElementById('status').textContent = 'Starting download'
 
     setTimeout(function () {
-        document.getElementById('status').textContent = 'Starting download'
+      document.getElementById('status').textContent = 'Downloading model 1'
+      const apiUrl = 'http://localhost:5000/api/status'
+      let model
 
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+          }
+          return response.json()
+        })
+        .then(data => {
+          model = data
+        })
+        .catch(error => {
+          console.error('Fetch error:', error)
+        })
+
+      for (let i = 0; i < 10; i++) {
         setTimeout(function () {
-            document.getElementById('status').textContent = 'Downloading model 1'
-            const apiUrl = 'http://localhost:5000/api/status';
-            let model;
-            
-            fetch(apiUrl)
+          fetch(apiUrl)
             .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
+              if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+              }
+              return response.json()
             })
             .then(data => {
-                model = data;
+              model = data
             })
             .catch(error => {
-                console.error('Fetch error:', error);
-            });
+              console.error('Fetch error:', error)
+            })
+        }, 1000)
+        i = i + 1
+      }
+      document.getElementById('status').textContent = 'All Models Downloaded'
 
-            for (let i = 0; i < 10; i++){
-                setTimeout(function () {
-                    fetch(apiUrl)
-                    .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    return response.json();
-                    })
-                    .then(data => {
-                        model = data;
-                    })
-                    .catch(error => {
-                        console.error('Fetch error:', error);
-                    });
-                }, 1000)
-                i = i + 1;
-            }
-            document.getElementById('status').textContent = 'All Models Downloaded'
+      setTimeout(function () {
+        document.getElementById('status').textContent = 'Setting up'
 
-            setTimeout(function () {
-                document.getElementById('status').textContent = 'Setting up'
-                
-                setTimeout(function () {
-                    document.getElementById('status').textContent = 'Done!'
-                }, 50)
-            }, 50)
+        setTimeout(function () {
+          document.getElementById('status').textContent = 'Done!'
         }, 50)
+      }, 50)
     }, 50)
-  }
+  }, 50)
+}
 
-  placeholderPlaygroundInitialization()
+placeholderPlaygroundInitialization()
