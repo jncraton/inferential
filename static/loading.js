8736c4c08a92a504ed69dba1efe89842bb5f6640
shama2025 <2003mashaffer@gmail.com>
2023-11-30 01:39:49 -0500

    Updated model load display

--- static/loading.js
+++ static/loading.js
@@ -4,13 +4,20 @@ async function updateData() {
   let models_status = await fetch('/api/status').then(response =>
     response.json(),
   )
+  //Get the number of models
+  let size = Object.keys(models_status.models).length
 
   // TODO (PR#57): Make the data in models_status more readable for the status page.
-  statusElement.innerText = JSON.stringify(
-    models_status,
-    undefined,
-    2,
-  ).replaceAll(' ', '\u00a0')
+
+ 
+  statusElement.innerHTML = " "
+    for(let i = 0; i < size; i++){
+      if(models_status.models[i].loaded){
+        statusElement.innerHTML += JSON.stringify(models_status.models[i].name).replace(/(['"])/g, "") + " is loaded!" + "<br>"
+      }
+      statusElement.innerHTML += JSON.stringify(models_status.models[i].name).replace(/(['"])/g, "") + " is loading! <br>"
+    }
+
 
   if (!models_status.loadedAll) window.setTimeout(updateData, 5000)
 }
