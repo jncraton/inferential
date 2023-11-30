4b526aa64ee310574cf4e2d571259a2c8a530712
shama2025 <2003mashaffer@gmail.com>
2023-11-30 01:50:36 -0500

    Revert "Updated model load display"
    This reverts commit 8736c4c08a92a504ed69dba1efe89842bb5f6640.

--- static/loading.js
+++ static/loading.js
@@ -4,20 +4,13 @@ async function updateData() {
   let models_status = await fetch('/api/status').then(response =>
     response.json(),
   )
-  //Get the number of models
-  let size = Object.keys(models_status.models).length
 
   // TODO (PR#57): Make the data in models_status more readable for the status page.
-
- 
-  statusElement.innerHTML = " "
-    for(let i = 0; i < size; i++){
-      if(models_status.models[i].loaded){
-        statusElement.innerHTML += JSON.stringify(models_status.models[i].name).replace(/(['"])/g, "") + " is loaded!" + "<br>"
-      }
-      statusElement.innerHTML += JSON.stringify(models_status.models[i].name).replace(/(['"])/g, "") + " is loading! <br>"
-    }
-
+  statusElement.innerText = JSON.stringify(
+    models_status,
+    undefined,
+    2,
+  ).replaceAll(' ', '\u00a0')
 
   if (!models_status.loadedAll) window.setTimeout(updateData, 5000)
 }
