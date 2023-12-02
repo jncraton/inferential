9e41dd60cf0f71a7bb28c90e86f8d44fcb27e383
Skylar <scoutskylar@outlook.com>
2023-12-02 12:32:25 -0500

    Clean up model loading (#97)
    * Move model loading to inference module
    
    * Skip reloading tokenizer
    
    * Instantiate model properties from config
    
    * Simplify model access
    
    * Check if model loaded before generating
    
    * Fix status endpoint
    
    * Replace invalid model check
    
    * Simplify iteration during model init
    
    * Skip explicitly storing model status
    
    * Combine generation functions
    
    * Use f-string for error message
    
    * Remove duplicate model_status structure
    
    * Iterate over models directly when loading
    
    * Get logo location from config object as needed
    
    * Simplify long prompt detection
    
    ---------
    
    Co-authored-by: Jon Craton <jncraton@gmail.com>
    Co-authored-by: shama2025 <2003mashaffer@gmail.com>
    Co-authored-by: Josh Westrum <jcwestrum@anderson.edu>

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
-import threading
 import yaml
 from flask import Flask, Response, request, render_template, send_from_directory
-from inference import (
-    generate_response_ctranslate2,
-    generate_response_ctransformers,
-    download_llms,
-)
+from inference import generate, models
 
 app = Flask(__name__)
 
-# Opens the config file and sets up config_models
-models_status = {"models": [], "loadedAll": False}
-with open("config.yml", "r") as f:
-    config_root = yaml.safe_load(f)
-    config_models = config_root["models"]
-    logo = config_root["logo"]
-for model in config_models:
-    models_status["models"].append({"name": model["name"], "loaded": False})
 
-# Start a new thread to load the models asynchronously
-models = {}
-threading.Thread(
-    target=download_llms, args=(config_models, models, models_status)
-).start()
+# Read the config file
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
 
 
 # Loading page
 @app.route("/")
 def loading_page():
-    return render_template("status.html", logo=logo)
+    return render_template("status.html", logo=config["logo"])
 
 
 # API Front End
 @app.route("/playground")
 def playground():
-    return render_template("index.html", models=config_models, logo=logo)
+    return render_template("index.html", models=config["models"], logo=config["logo"])
 
 
 @app.route("/favicon.ico")
@@ -51,30 +36,20 @@ def api():
     if request.args.get("model", ""):
         model_name = request.args.get("model", "")
     else:
-        model_name = config_models[0]["name"]
-    if not model_name in models:
-        return "Error: Unknown model name '" + model_name + "'.", 400  # 400 Bad Request
-    model_config = models[model_name]
+        model_name = config["models"][0]["name"]
 
-    for model in config_models:
-        if model["name"] == model_name:
-            maxPrompt = model["maxPromptToken"]
     if query == "":
         return "Error: No prompt was provided.", 400  # 400 Bad Request
-    if len(query) >= maxPrompt:
+    if not model_name in models:
+        return f"Error: Unknown model name '{model_name}'.", 400  # 400 Bad Request
+    if len(query) >= models[model_name]["maxPromptToken"]:
         return "Error: The prompt was too long.", 413  # 413 Content Too Large
 
-    if model_config["backend"] == "ctransformers":
-        reply = generate_response_ctransformers(query, model_config["auto-model"])
-    elif model_config["backend"] == "ctranslate2":
-        reply = generate_response_ctranslate2(query, model_config["model-path"])
-    else:
-        raise ValueError(
-            "Invalid backend in loaded models list for model named '" + model_name + "'"
-        )
-    return Response(reply, content_type="text/plain")
+    return Response(generate(query, model_name), content_type="text/plain")
 
 
 @app.route("/api/status")
 def api_status_page():
-    return models_status
+    status = [{"name": m["name"], "loaded": "model" in m} for m in models.values()]
+
+    return {"models": status, "loadedAll": all(m["loaded"] for m in status)}
