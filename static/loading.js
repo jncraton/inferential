6efaaac63c9b6638979d840287eaea42baf37d82
Jon Craton <jncraton@gmail.com>
2023-12-18 16:42:45 -0500

    Add spinner and checkmarks to status page

--- static/loading.js
+++ static/loading.js
@@ -1,16 +1,13 @@
 // This script fetches and displays the status of pre-trained models from the API, indicating whether each model is ready or not. It dynamically updates the HTML content with the current status and sets a periodic refresh to keep the information up to date.
 
-let statusElement = document.getElementById('status')
-
 async function updateData() {
-  let models_status = await fetch('/api/status').then(response =>
-    response.json(),
-  )
+  let response = await fetch('/api/status')
+  let status = await response.json()
 
-  statusElement.innerHTML = models_status.models
-    .map(model => `${model.name}: ${model.loaded ? 'Ready' : 'Not ready'}`)
-    .join('<br>')
+  document.getElementById('status').innerHTML = status.models
+    .map(m => `<p>${m.loaded ? '✔' : '❌'} ${m.name}`)
+    .join('')
 
-  if (!models_status.loadedAll) window.setTimeout(updateData, 5000)
+  if (!status.loadedAll) setTimeout(updateData, 5000)
 }
 updateData()
